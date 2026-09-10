c97f7184535ee04bb983742635160a372eeec1ee
pubgluafile <pubgluafile@gmail.com>
2026-09-10 15:16:24 +0500

    Modify UXOFFICIAL settings in keylist.lua
    Updated expiry date and max devices for UXOFFICIAL.

--- keylist.lua
+++ keylist.lua
@@ -19,9 +19,9 @@ return {
     },
     ["UXOFFICIAL"] = {
         type = "DEMO",
-        expiry = "2026-09-10",
+        expiry = "2026-09-11",
         valid = true,
-        max_devices = 500,
+        max_devices = 10,
         SLOT = "3"
     },
     ["LEGENDVIP"] = {
