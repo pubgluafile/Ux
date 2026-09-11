17011a9a53a5f834c438412c0978b5561db28446
pubgluafile <pubgluafile@gmail.com>
2026-09-11 20:17:24 +0500

    Increase max_devices for Asifkhan60DayUx to 2

--- keylist.lua
+++ keylist.lua
@@ -14,7 +14,7 @@ return {
         type = "DEMO",
         expiry = "2026-11-09",
         valid = true,
-        max_devices = 1,
+        max_devices = 2,
         SLOT = "2"
     },
     ["UXOFFICIAL"] = {
