0ebeb349a936bd2a3c77b3778b05eed06cfe08af
pubgluafile <pubgluafile@gmail.com>
2026-09-13 03:29:45 +0500

    Update keylist.lua

--- keylist.lua
+++ keylist.lua
@@ -133,7 +133,7 @@ return {
         type = "DEMO",
         expiry = "2026-10-11",
         valid = true,
-        max_devices = 1,
+        max_devices =2,
         SLOT = "19"
     },
     ["VIP_1Day_IFI38U2JEU3"] = {
