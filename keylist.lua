b7fc4b322ddd4c056abc29f3a744e3eb11d84cfc
pubgluafile <pubgluafile@gmail.com>
2026-09-13 03:09:12 +0500

    Add VIP_1Day_UDYEHAJH key with expiry and slot

--- keylist.lua
+++ keylist.lua
@@ -143,11 +143,18 @@ return {
         max_devices = 2,
         SLOT = "20"
     },
+    ["VIP_1Day_UDYEHAJH"] = {
+        type = "DEMO",
+        expiry = "2026-9-14",
+        valid = true,
+        max_devices = 1,
+        SLOT = "21"
+    },
         ["LEGEND-30D-HW727S83R"] = {
         type = "DEMO",
         expiry = "2026-10-11,
         valid = true,
         max_devices = 10,
-        SLOT = "21"
+        SLOT = "22"
     }
 }
