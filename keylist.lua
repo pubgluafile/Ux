404f316cbb727c7db7e07c9b0fab1b2f0eaf750c
pubgluafile <pubgluafile@gmail.com>
2026-09-13 18:17:21 +0500

    Update keylist.lua

--- keylist.lua
+++ keylist.lua
@@ -163,12 +163,46 @@ return {
         valid = true,
         max_devices = 1,
         SLOT = "23"
+    },["VIP_1Day_KDHE736SWYY"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "24"
+    },
+    ["VIP_7Day_MAJE3YU2UEH"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "25"
+    },
+    ["VIP_7Day_MAJE3YU2UEH"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "26"
+    },
+    ["VIP_7Day_MAJE3YU2UEH"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "27"
+    },
+    ["VIP_7Day_MAJE3YU2UEH"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "28"
     },
         ["LEGEND-30D-HW727S83R"] = {
         type = "DEMO",
         expiry = "2026-10-11,
         valid = true,
         max_devices = 10,
-        SLOT = "24"
+        SLOT = "29"
     }
 }
