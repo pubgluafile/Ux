4b7a2e8ef9d235904b9986d2950c9738dde7642b
pubgluafile <pubgluafile@gmail.com>
2026-09-13 15:03:59 +0500

    Add VIP_7Day_MAJE3YU2UEH key configuration

--- keylist.lua
+++ keylist.lua
@@ -157,11 +157,18 @@ return {
         max_devices = 1,
         SLOT = "22"
     },
+    ["VIP_7Day_MAJE3YU2UEH"] = {
+        type = "DEMO",
+        expiry = "2026-9-20",
+        valid = true,
+        max_devices = 1,
+        SLOT = "23"
+    },
         ["LEGEND-30D-HW727S83R"] = {
         type = "DEMO",
         expiry = "2026-10-11,
         valid = true,
         max_devices = 10,
-        SLOT = "23"
+        SLOT = "24"
     }
 }
